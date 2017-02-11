16807a691f6e68fdc9dab10383c07a6a2256d24e
Thirapat Nanongtum <thirapatn56>
2017-02-11 20:12:02 +0700

    ass_1 v2

--- getData.js
+++ getData.js
@@ -1,11 +1,8 @@
 $(document).ready(function(){
-    //ชือก็บอกว่า getjson จาก  url แล้วทำฟังก์ชัน result
     $("button").click(function(){
       $("div").empty();
         $.getJSON("https://jsonplaceholder.typicode.com/users?username="+$("#username").val(), function(result){
-            //เอา result ใส่ field
             $.each(result, function(i, field){
-              // อ่าน result ใน field
                 $("div").append("<p>Name : " + field.name + "</p>");
                 $("div").append("<p>Email : " + field.email + "</p>");
                 $("div").append("<p>Address : " + field.address.street + " " + field.address.suite + " " + field.address.city + " " + field.address.zipcode + "</p>");
